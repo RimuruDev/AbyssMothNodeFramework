2dfbce4fbd244a02b43f09a63b5211145861b58d
RimuruDev <Rimuru.Dev@gmail.com>
2026-02-13 23:26:39 +0400

    Bind Project Context

--- Runtime/ProjectContext/ProjectRootConnector.cs
+++ Runtime/ProjectContext/ProjectRootConnector.cs
@@ -1,3 +1,7 @@
+#if UNITY_EDITOR
+#define UNITY_EDITOR_MODE
+#endif
+
 using UnityEngine;
 using UnityEngine.Scripting;
 
@@ -11,14 +15,18 @@ namespace AbyssMoth
     {
         public ServiceRegistry ProjectContext { get; private set; }
 
+#if UNITY_EDITOR_MODE
         public override void OnValidate()
         {
             Order = -1;
             base.OnValidate();
         }
+#endif
 
         private void Awake()
         {
+            ProjectRootRegistry.Set(root: this);
+            
             ProjectContext = new ServiceRegistry();
 
             Debug.Log($"<color=magenta> <color=red>></color> ProjectRootConnector.Execute()</color>");
@@ -26,6 +34,14 @@ namespace AbyssMoth
 
             transform.SetParent(null);
             DontDestroyOnLoad(gameObject);
+        }
+
+        public override void OnDestroy()
+        {
+            if (!Application.isPlaying)
+                return;
+
+            ProjectRootRegistry.Clear(root: this);
         }
     }
 }
